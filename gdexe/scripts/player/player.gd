715d7b939f9dbde4667f1b6fd4336d0b016033d6
Tomic <tomic@tomic.cc>
2024-11-08 18:02:25 +0100

    Added fpcam.

--- gdexe/scripts/player/player.gd
+++ gdexe/scripts/player/player.gd
@@ -1,15 +1,17 @@
 extends RigidBody3D
 
 @export var walk_speed = 3.0
-@export var walk_accel_factor = 10.0
+@export var walk_accel_factor = 5.0
 @export var run_speed = 8.0
-@export var run_accel_factor = 20.0
+@export var run_accel_factor = 10.0
 @export var input_enabled = true
 @export var is_running = false
 @export var is_running_default_state = true
 @onready var _downcast = $DownCast
-var _input = PlayerInput.new()
-
+@onready var jump_impulse = 20000
+var can_jump = true
+@onready var _input = $PlayerInput
+@onready var _forward = $ForwardRef
 func is_on_ground():
 	return _downcast.is_colliding()
 
@@ -29,14 +31,15 @@ func _process(delta: float) -> void:
 			else:
 				movement_speed = walk_speed
 				movement_accel_factor = walk_accel_factor
-			var mvmtDir = Vector3(_input.movementInput.x,0.0 ,  _input.movementInput.y)
-			print("mvmtDir * movement_speed ", (mvmtDir * movement_speed), "v: ", self.linear_velocity)
-			var vDelta = (mvmtDir * movement_speed - self.linear_velocity).length() #TODO: Fix movement being sluggish when reversing direction.
-			#TODO: add graphing stuff 
-			if(vDelta < 0.0):
-				vDelta = 0.0
-			print("vDelta: ", vDelta, " movInp: ", _input.movementInput, " v: ", self.linear_velocity)
-			var moveForce = mvmtDir * movement_accel_factor * vDelta
-			print(moveForce)
-			self.apply_force(moveForce * self.mass); #mass independent
-		
+			var mvmtDir = _forward.transform.basis * Vector3(_input.movementInput.x,0.0 ,  _input.movementInput.y)
+			var vDelta = (mvmtDir * movement_speed - self.linear_velocity)
+			vDelta = Vector3(vDelta.x, 0.0 ,vDelta.z)
+			if(vDelta.length() < 0.0):
+				vDelta = Vector3.ZERO
+			var moveForce = movement_accel_factor * vDelta
+			if _input.jumpButton and can_jump:
+				moveForce.y = jump_impulse / self.mass
+				can_jump = false
+			self.apply_force(moveForce * self.mass ); #mass independent
+		else:
+			can_jump = true
--- gdexe/scripts/player/player_input.gd
+++ gdexe/scripts/player/player_input.gd
@@ -1,7 +1,26 @@
+extends Node
 class_name PlayerInput
 var movementInput = Vector2(0.0,0.0)
+var lookInput = Vector2(0.0,0.0)
+var _mouseLook = Vector2(0.0, 0.0)
 var movementModeButton = false
+var jumpButton = false
+var mouseSensitivity = 1.0
+@onready var curPos = get_viewport().get_mouse_position()
+@onready var _lastPos = get_viewport().get_mouse_position()
 
 func update():
-	movementInput =Input.get_vector("right","left","down","up")
+	movementInput = Input.get_vector("right","left","down","up")
 	movementModeButton = Input.is_action_pressed("mvntMode")
+	jumpButton = Input.is_action_pressed("jump")
+	lookInput = Vector2.ZERO
+	lookInput = Input.get_vector("look_right", "look_left", "look_down", "look_up") + _mouseLook
+
+func _process(delta: float) -> void:
+	_mouseLook = (curPos - _lastPos) * mouseSensitivity / 10.0
+	_mouseLook.x = -_mouseLook.x
+	_lastPos = curPos
+	
+func _input(event: InputEvent) -> void:
+	if(event is InputEventMouseMotion):
+		curPos = curPos + event.relative
